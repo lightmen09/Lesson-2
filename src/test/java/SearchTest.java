4010aea0ff141ac8dc274d04506eb8299d7a66e1
Mikhail Andreev <lightmen09@gmail.com>
2025-06-15 21:23:42 +0300

    Add files after changes

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -5,10 +6,19 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
+
     @Test
     void successfulSearchTest() {
-        open("https://www.google.com/");
+        Configuration.pageLoadStrategy = "eager";
+        open("https://bing.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        $("html").shouldHave(text("https://selenide.org"));
+    }
+    @Test
+    void successfulSearchTestNew() {
+        Configuration.pageLoadStrategy = "eager";
+        open("https://bing.com/");
+        $("[name=q]").setValue("yandex").pressEnter();
+        $("html").shouldHave(text("https://ya.ru"));
     }
 }
